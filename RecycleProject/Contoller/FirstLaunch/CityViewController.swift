4cc67aa5541d545cd377da7189e40bc50cdf1096
artemiy <->
2020-05-10 00:06:25 +0300

    Create transition to main screen

--- RecycleProject/Contoller/FirstLaunch/CityViewController.swift
+++ RecycleProject/Contoller/FirstLaunch/CityViewController.swift
@@ -16,6 +16,14 @@ class CityViewController: UIViewController {
     }
     @IBAction func doneButtonTapped(_ sender: UIButton) {
         UserDefaults.standard.setLaunchedBefore(value: true)
+        
+        DispatchQueue.main.async {
+            let sb = UIStoryboard(name: "Main", bundle: nil)
+            let mainVC = sb.instantiateInitialViewController() as! UITabBarController
+            mainVC.modalPresentationStyle = .fullScreen
+            self.present(mainVC, animated: false)
+            
+        }
     }
     
 }
