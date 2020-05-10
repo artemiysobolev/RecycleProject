44a64631222c8bd40d17c11e3e0138c867d382f7
artemiy <->
2020-05-10 18:04:29 +0300

    Change settings for show launch view controller

--- RecycleProject/Contoller/NewsViewController.swift
+++ RecycleProject/Contoller/NewsViewController.swift
@@ -10,15 +10,6 @@ class NewsViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        guard UserDefaults.standard.isLaunchedBefore() else {
-            DispatchQueue.main.async {
-                let sb = UIStoryboard(name: "FirstLaunch", bundle: nil)
-                let launchVC = sb.instantiateViewController(withIdentifier: "FirstLaunchViewController") as! FirstLaunchViewController
-                launchVC.modalPresentationStyle = .fullScreen
-                self.present(launchVC, animated: false)
-            }
-            return
-        }
         print("NewsViewController did load")
     }
     
