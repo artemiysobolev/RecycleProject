ba8d3b2d0c7e8fbc2cbe78685b44ac638148dbd5
artemiy <->
2020-05-15 21:37:59 +0300

    Create database model

--- RecycleProject/Supporting files/AppDelegate.swift
+++ RecycleProject/Supporting files/AppDelegate.swift
@@ -10,7 +10,7 @@ import Firebase
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
+    var coreDataStack = CoreDataStack()
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         FirebaseApp.configure()
