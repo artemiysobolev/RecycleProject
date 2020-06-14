fbbaa93fbd80580397bcb3519159ed22b8d47c1e
artemiy <->
2020-06-14 19:10:55 +0300

    Bug fix

--- RecycleProject/Flow/Map/View/MaterialCollectionViewCell.swift
+++ RecycleProject/Flow/Map/View/MaterialCollectionViewCell.swift
@@ -10,4 +10,9 @@ class MaterialCollectionViewCell: UICollectionViewCell {
     @IBOutlet weak var recycleCodeLabel: UILabel!
     @IBOutlet weak var materialNameLabel: UILabel!
     
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        recycleCodeLabel.text = nil
+        materialNameLabel.text = nil
+    }
 }
